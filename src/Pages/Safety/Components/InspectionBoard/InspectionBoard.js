d3f848c381cf175d35d998d3b413e49aa82499fd
Evans <evans@vorane.com>
2020-08-17 15:56:12 +0300

    TCW add draggable listis

--- src/Pages/Safety/Components/InspectionBoard/InspectionBoard.js
+++ src/Pages/Safety/Components/InspectionBoard/InspectionBoard.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 import styled from "styled-components";
 import { Card, Icon, Image } from "semantic-ui-react";
-
+import initialData from "./data";
 
 const Wrapper = styled.div`
   flex: 1;
   display: flex;
   flex-direction: row;
-  overflow-x: scroll;
 `;
 
 const StatusColumn = styled.div`
@@ -25,75 +25,62 @@ const CardsContainer = styled.div``;
 
 const ColumnHeader = styled.h4``;
 
-const StatusCard = () => (
-  <Card>
-    <Card.Content>
-      <Card.Header>Daniel</Card.Header>
-      <Card.Meta>Joined in 2016</Card.Meta>
-      <Card.Description>
-        Daniel is a comedian living in Nashville.
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name="user" />
-        10 Friends
-      </a>
-    </Card.Content>
-  </Card>
+const StatusCard = ({ task, index }) => (
+  <Draggable draggableId={task.id} index={index}>
+    {(provided) => (
+      <div
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+        ref={provided.innerRef}
+      >
+        <Card>
+          <Card.Content>
+            <Card.Header>{task.name}</Card.Header>
+            <Card.Meta>{task.date}</Card.Meta>
+          </Card.Content>
+        </Card>
+      </div>
+    )}
+  </Draggable>
 );
 
+const Column = ({ column, tasks }) => {
+  return (
+    <StatusColumn>
+      <ColumnHeaderContainer>
+        <ColumnHeader>{column.title}</ColumnHeader>
+      </ColumnHeaderContainer>
+      <Droppable droppableId={column.id}>
+        {(provided) => (
+          <CardsContainer
+            innerRef={provided.innerRef}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            {tasks.map((item, index) => (
+              <StatusCard index={index} key={item.id} task={item} />
+            ))}
+            {provided.placeholder}
+          </CardsContainer>
+        )}
+      </Droppable>
+    </StatusColumn>
+  );
+};
+
 export default function InspectionBoard() {
+  let [data, setData] = useState(initialData);
+
+  const onDragEnd = (result) => {};
   return (
     <Wrapper>
-      <StatusColumn>
-        <ColumnHeaderContainer>
-          <ColumnHeader>INSPECTION NEEDED</ColumnHeader>
-        </ColumnHeaderContainer>
-        <CardsContainer>
-          {Array(15)
-            .fill({ title: "Title" })
-            .map((item, index) => (
-              <StatusCard key={index} />
-            ))}
-        </CardsContainer>
-      </StatusColumn>
-      <StatusColumn>
-        <ColumnHeaderContainer>
-          <ColumnHeader>INSPECTION NEEDED</ColumnHeader>
-        </ColumnHeaderContainer>
-        <CardsContainer>
-          {Array(15)
-            .fill({ title: "Title" })
-            .map((item, index) => (
-              <StatusCard key={index} />
-            ))}
-        </CardsContainer>
-      </StatusColumn>
-      <StatusColumn>
-        <ColumnHeaderContainer>
-          <ColumnHeader>INSPECTION NEEDED</ColumnHeader>
-        </ColumnHeaderContainer>
-        <CardsContainer>
-          {Array(15)
-            .fill({ title: "Title" })
-            .map((item, index) => (
-              <StatusCard key={index} />
-            ))}
-        </CardsContainer>
-      </StatusColumn>
-      <StatusColumn>
-        <ColumnHeaderContainer>
-          <ColumnHeader>INSPECTION NEEDED</ColumnHeader>
-        </ColumnHeaderContainer>
-        <CardsContainer>
-          {Array(15)
-            .fill({ title: "Title" })
-            .map((item, index) => (
-              <StatusCard key={index} />
-            ))}
-        </CardsContainer>
-      </StatusColumn>
+      <DragDropContext onDragEnd={onDragEnd}>
+        {data.columnOrder.map((columnId) => {
+          let column = data.columns[columnId];
+          let tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
+          return <Column column={column} tasks={tasks} />;
+        })}
+      </DragDropContext>
     </Wrapper>
   );
 }
---
+++ src/Pages/Safety/Components/InspectionBoard/data.js
@@ -0,0 +1,63 @@
+export default {
+  tasks: {
+    "task-1": {
+      id: "task-1",
+      content: "Take out grabaget",
+      date: "June 12",
+      name: "Jacob Jone",
+      equipmentNumber: 123,
+      status: "inspection needed",
+      color: "#f15f41",
+    },
+    "task-2": {
+      id: "task-2",
+      content: "Take out grabaget",
+      date: "June 12",
+      name: "James Jone",
+      equipmentNumber: 123,
+      status: "inspection needed",
+      color: "#f15f41",
+    },
+    "task-3": {
+      id: "task-3",
+      content: "Take out grabaget",
+      date: "June 12",
+      name: "July Jone",
+      equipmentNumber: 123,
+      status: "inspection needed",
+      color: "#f15f41",
+    },
+    "task-4": {
+      id: "task-4",
+      content: "Take out grabaget",
+      date: "June 12",
+      name: "John Jone",
+      equipmentNumber: 123,
+      status: "inspection needed",
+      color: "#f15f41",
+    },
+    "task-5": {
+      id: "task-5",
+      content: "Take out grabaget",
+      date: "June 12",
+      name: "Jacob Jone",
+      equipmentNumber: 123,
+      status: "inspection needed",
+      color: "#f15f41",
+    },
+  },
+  columns: {
+    "column-1": {
+      id: "column-1",
+      title: "Inspection needed",
+      taskIds: ["task-1", "task-2", "task-3", "task-5"],
+    },
+    "column-2": {
+      id: "column-2",
+      title: "Assigned",
+      taskIds: ["task-4"],
+    },
+  },
+
+  columnOrder: ["column-1", "column-2"],
+};
